931b3787897223b8916e0d57e7debc98eeab2c03
Kayleigh Knapp <kayleigh.s.knapp@gmail.com>
2015-04-12 18:59:37 -0500

    it works

--- brewUpdater/main.swift
+++ brewUpdater/main.swift
@@ -8,5 +8,17 @@
 
 import Foundation
 
-println("Hello, World!")
+let updateCommand = "/usr/local/bin/brew update"
+let doctorCommand = "/usr/local/bin/brew doctor > /var/tmp/doctormessage"
+let success = "Your system is ready to brew.\n"
 
+system(updateCommand)
+system(doctorCommand)
+
+let doctorMessage = String(contentsOfFile: "/var/tmp/doctormessage", encoding: NSUTF8StringEncoding, error: nil)
+
+if let message = doctorMessage {
+    if message != success {
+        NSTask.launchedTaskWithLaunchPath("/usr/bin/osascript", arguments: ["-e", "display notification \"\(message)\" with title \"brew update issue\""])
+    }
+}
